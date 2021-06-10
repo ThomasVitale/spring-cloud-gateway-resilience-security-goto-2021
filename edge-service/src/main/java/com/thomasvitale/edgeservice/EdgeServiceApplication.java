662824da77527584d1c5dd5bdd8dc0c683e88527
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-06-10 22:29:14 +0200

    Add security

--- edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
@@ -1,12 +1,14 @@
 package com.thomasvitale.edgeservice;
 
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,12 @@ public class EdgeServiceApplication {
 	}
 
 	@Bean
-	KeyResolver keyResolver() {
-		return exchange -> Mono.just("USER");
+	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		return http
+				.authorizeExchange(exchange -> exchange.matchers(EndpointRequest.toAnyEndpoint()).permitAll()
+						.anyExchange().authenticated())
+				.oauth2Login(Customizer.withDefaults())
+				.build();
 	}
 
 }
